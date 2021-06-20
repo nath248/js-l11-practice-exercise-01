cdde4a44e361e97e76aa2f84be7d036b81c50a10
nath248 <nmherrera20@outlook.com>
2021-06-20 10:29:56 -0400

    Add javascript code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function() {
+    const usersRequest = await fetch(
+        "https://randomuser.me/api?results=5"
+    );
+    const data = await usersRequest.json();
+    const usersResults = data.results;
+    displayUsers(usersResults);
+};
+
+getData();
+
+const displayUsers = function(usersResults) {
+    randomFolks.innerHTML = "";
+
+    for (const user of usersResults) {
+        const country = user.location.country;
+        const name = user.name.first;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+        randomFolks.append(userDiv);
+    }
+};
